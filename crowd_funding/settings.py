11dad277b63def3519d41edff6f03e10e5af81b9
Seleem1010 <mohammedseleem10101997@gmail.com>
2023-06-09 21:13:25 +0300

    user model

--- crowd_funding/settings.py
+++ crowd_funding/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_profile.apps.UserProfileConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,14 @@ WSGI_APPLICATION = 'crowd_funding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'crowdfund',
+        'USER':'postgres',
+        'PASSWORD':'Mo#Selim',
+        'HOST':'127.0.0.1',
+        'PORT':5432
     }
 }
 
