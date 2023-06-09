f3707eece525e9808f986f557cbbc44ba1e73dd8
Seleem1010 <mohammedseleem10101997@gmail.com>
2023-06-09 23:29:23 +0300

    add all models v1

---
+++ project/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from user_profile.models import *
+from django.db.models import Avg
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    details = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    target = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    owener_email = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_rate = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+
+    def calculate_total_rating(self):
+        ratings = Rating.objects.filter(project=self)
+        if ratings.exists():
+            self.total_rate = ratings.aggregate(Avg('rate'))['rate__avg']
+        else:
+            self.total_rate = 0
+
+
+class Tag(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tag_name = models.CharField(max_length=50)
+
+class Image(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='project_pictures')
+
+class Donation(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_email = models.ForeignKey(User, on_delete=models.CASCADE)
+    donate_amount = models.IntegerField()
+
+class Comment(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_email = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_body = models.TextField()
+    report_comment = models.CharField()
+    user_report = models.CharField()
+
+class Report(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_email = models.ForeignKey(User, on_delete=models.CASCADE)
+    report_body = models.TextField()
+    user_report = models.CharField()
+
+class Rating(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_email = models.ForeignKey(User, on_delete=models.CASCADE)
+    rate = models.IntegerField()
+    
+    def save(self, *args, **kwargs):
+        super(Rating, self).save(*args, **kwargs)
+        self.project.calculate_total_rating()
+        self.project.save()
+
