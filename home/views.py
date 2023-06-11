0dbb9040b0a331ee20d0b0f10900c8b5ba21c458
abdallah <abdallah011848@gmail.com>
2023-06-11 20:49:10 +0300

    Slider

--- home/views.py
+++ home/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse ,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from project.models import *
 from user_profile.models import *
 
 # Create your views here.
-def myHome(req):
-    return render(req,'home.html')
 
-def getCategory(req,str):
-    return render(req,'category.html')
+
+def myHome(req):
+    return render(req, 'home.html')
+
+
+def getCategory(req, str):
+    return render(req, 'category.html')
+
+
+def image_slider(request):
+    context = {}
+    images = Image.objects.all()
+    context['images'] = images
+    return images
