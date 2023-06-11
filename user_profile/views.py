9b0f2c7ef65c469067691759bdf62405654dc84e
amr alaa <amraalaa22@gmail.com>
2023-06-11 21:23:38 +0300

    added varibles name

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,17 +5,17 @@ from django.db.models import Sum
 # Create your views here.
 
 def listProject(request):
-    p=Projects.objects.filter(owner_id=2)
+    project_id=Projects.objects.filter(owner_id=2)
     sumlist = {}
-    for j in p :
+    for j in project_id :
         total=Donation.objects.filter(project=j.id)
         sum= 0
         for i in total:
             sum+=i.donate_amount
         sumlist[j.id]=sum
 
-    d=Donation.objects.filter(user_id=1)
-    context={'p':p,'d':d,'sumlist':sumlist}
+    donation_id=Donation.objects.filter(user_id=1)
+    context={'project_id':project_id,'donation_id':donation_id,'sumlist':sumlist}
     print(sumlist)
     return render(request,'user_profile/list_user_project.html',context)
 
